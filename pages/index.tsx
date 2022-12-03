aa483262e70a4b128136b01212041e39474abc57
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-12-03 13:05:35 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
-export const Navigation = () => {
+export const Ribbon = () => {
     return (
-        <section className="bg-green-700">
-            <span className="text-left text-white text-lg">Yuh</span>
+        <section className="bg-green-500 text-orange-200 text-lg text-center font-bold sticky">
+            <span>Welcome to my website!</span>
         </section>
+    )
+}
+
+export const Intro = () => {
+    return (
+        <p className="text-gray-200 mx-32">
+            Hello
+        </p>
     )
 }
 
 const Index = () => {
     return (
-        <main className='bg-black bg-blend-color-burn'>
-            <Navigation />
+        <main className="bg-black bg-blend-color-burn">
+            <Ribbon />
+            <Intro />
         </main>
     )
 }; export default Index
