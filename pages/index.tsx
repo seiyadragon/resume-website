199b2e1f0865574a57f6aa4471fb496aa23e5fef
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-12-03 12:27:25 -0600

    0.0.1

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+export const Navigation = () => {
+    return (
+        <section className="bg-green-700">
+            <span className="text-left text-white text-lg">Yuh</span>
+        </section>
+    )
+}
+
+const Index = () => {
+    return (
+        <main className='bg-black bg-blend-color-burn'>
+            <Navigation />
+        </main>
+    )
+}; export default Index
